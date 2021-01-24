66dd5e03377d795bd1bc82ba105ab6eb15dcb80b
sebbastien <sebastien.leclech@gmail.com>
2021-01-24 15:52:18 +0100

    update

--- check_files.sh
+++ check_files.sh
@@ -8,6 +8,25 @@ else
    echo "Well... apparently $diff files are missing. All the csv files should be in data/sampled"
    echo "Please make sure you execute this notebook in the parent folder."
    echo "You are here: `pwd` "
-   echo "If you need, run the script download_metadata.sh to download and set the folders."
-   echo "------------ ERROR -----------"
+   echo -n "Do you want I help you to download the data (y/n)? "
+   read answer
+   if [ "$answer" != "${answer#[Yy]}" ] ; then
+       ./download_and_check_data.sh
+   else 
+       echo "If you need, run the script download_and_check_data.sh to download and set the folders."
+       echo "and try again"
+   fi 
+fi
+
+mkdir data/metadata &>/dev/null
+checkfile=`ls data/metadata | wc -l`
+if [ $checkfile != 13 ] ; then
+  echo "I will now download some documents to help you understand the data."
+  cat metadata_urls.txt | xargs -n 1 -P 6 wget -c -P data/metadata/ &>/dev/null
+  cd data/metadata
+  unzip taxi_zones.zip &>/dev/null && rm -f *.zip
+  cd ../..
+  echo "------------ READY TO START -----------"
+elif [ $checkfile == 13 ] ; then
+   echo "------------ READY TO START -----------"
 fi
